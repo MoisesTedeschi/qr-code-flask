278d9dd2e09cb1399d1703ba29f166b5d7ea7899
Moisés Tedeschi <moisestedeschi@gmail.com>
2020-08-17 15:22:55 -0300

    Metodos implementatos e ediçao do CSS - Finalizados.

--- app.py
+++ app.py
@@ -1,28 +1,32 @@
+from flask.globals import session
 from qr import qrcode_genetator
 from flask import Flask, render_template, send_file, request
 
 
 app = Flask(__name__)
 
-
+@app.route('/')
 @app.route('/qr-code-generator')
 def home():
     return render_template('index.html')
 
 
-@app.route('/converterd', methods = ['POST'])
+@app.route('/converted', methods = ['GET', 'POST'])
 def convert():
     global info_input
-    info_input = request.form['test']
-    return render_template('converted.html')
 
+    if request.method == "POST":
+        info_input = f"{request.form.get('valor_entrada')}"
+        return render_template('converted.html')
+    
+    return render_template('converted.html')
 
 @app.route('/download')
 def download():
     qrcode_genetator(info_input)
-    filename = f'{info_input}.png'
+    filename = info_input+'.png'
     return send_file(filename, as_attachment=True)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
--- qr.py
+++ qr.py
@@ -3,4 +3,6 @@ import pyqrcode
 
 def qrcode_genetator(info):
     qrcode = pyqrcode.create(info)
-    qrcode.png(info+'.png', scale=8)
+    #qrcode.svg (info+'.svg', scale=8)
+    #qrcode.svg (info+'.eps', scale=2)
+    qrcode.png(info+'.png', scale = 6)
